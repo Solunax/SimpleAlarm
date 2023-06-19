c5ce41833132beef2a3e4b12adaceff6c7253d09
Solunax <rudgh1409@gmail.com>
2023-06-19 20:34:27 +0900

    사용자가 보기로 지정한 세계시간을 sharedPreference 에 저장하는 기능 추가

--- app/src/main/java/com/example/simplealarm/fragment/WorldTimeFragment.kt
+++ app/src/main/java/com/example/simplealarm/fragment/WorldTimeFragment.kt
@@ -1,11 +1,16 @@
 package com.example.simplealarm.fragment
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.simplealarm.worldTimeRecycler.CountryTime
@@ -15,6 +20,9 @@ import com.example.simplealarm.databinding.WorldTimeFragmentBinding
 import com.example.simplealarm.dialog.CountryAddDialog
 import com.example.simplealarm.dialog.CountryAddInterface
 import com.example.simplealarm.worldTimeRecycler.WorldTimeRecyclerClickCallback
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import kotlin.concurrent.timer
@@ -23,9 +31,10 @@ import kotlin.concurrent.timer
 class WorldTimeFragment : Fragment(), CountryAddInterface, WorldTimeRecyclerClickCallback {
     private var binding: WorldTimeFragmentBinding? = null
     private val recyclerAdapter = WorldTimeRecyclerAdapter(this)
-    private val countryData = ArrayList<CountryTime>()
+    private var countryData = ArrayList<CountryTime>()
     private lateinit var country: Array<String>
     private val df = SimpleDateFormat("ss")
+    private lateinit var sharedPreferences : SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -60,6 +69,16 @@ class WorldTimeFragment : Fragment(), CountryAddInterface, WorldTimeRecyclerClic
         return binding!!.root
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sharedPreferences = requireActivity().getSharedPreferences("country", MODE_PRIVATE)
+        loadSharedPreference()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
     override fun onPositive(index: Int) {
         val timezone = when (index) {
             0 -> "Asia/Seoul"
@@ -68,10 +87,23 @@ class WorldTimeFragment : Fragment(), CountryAddInterface, WorldTimeRecyclerClic
             else -> ""
         }
 
-        if (timezone != "") {
-            countryData.add(CountryTime(country[index], timezone))
-            recyclerAdapter.setData(countryData)
+        var check = true
+        for(data in countryData){
+            if(data.timeZone == timezone){
+                check = false
+                break
+            }
         }
+
+        if(check){
+            if (timezone != "") {
+                countryData.add(CountryTime(country[index], timezone))
+                recyclerAdapter.setData(countryData)
+            }
+
+            saveSharedPreference()
+        }else
+            Toast.makeText(requireContext(), "이미 존재하는 세계시간입니다.", Toast.LENGTH_SHORT).show()
     }
 
     override fun onClickDelete(name: String) {
@@ -84,6 +116,25 @@ class WorldTimeFragment : Fragment(), CountryAddInterface, WorldTimeRecyclerClic
         }
 
         countryData.removeAt(index)
+        saveSharedPreference()
         recyclerAdapter.setData(countryData)
+    }
+
+    private fun saveSharedPreference(){
+        val edit = sharedPreferences.edit()
+        val gson = GsonBuilder().create()
+
+        edit.putString("country", gson.toJson(countryData))
+        edit.apply()
+    }
+
+    private fun loadSharedPreference(){
+        val loadData = sharedPreferences.getString("country", "")
+        val typeData = object : TypeToken<ArrayList<CountryTime>>(){}.type
+
+        if(loadData != ""){
+            countryData = Gson().fromJson(loadData, typeData)
+            recyclerAdapter.setData(countryData)
+        }
     }
 }
